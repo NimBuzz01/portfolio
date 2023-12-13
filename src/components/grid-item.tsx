e2054edc0099a28b295439923f0ac22a6fd444b1
niamatmarjan <86093827+NimBuzz01@users.noreply.github.com>
2023-12-13 18:55:18 +0530

    feat: add framer motion

--- src/components/grid-item.tsx
+++ src/components/grid-item.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { animate, AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
 
 const variants = cva(
   "shadow-md rounded-xl bg-white dark:bg-neutral-900 flex flex-col justify-center @container",
@@ -25,18 +29,45 @@ export type GridItemProps = { children: React.ReactNode } & VariantProps<
 >;
 
 const GridItem = ({ size, children }: GridItemProps) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div
-      className={cn(
-        variants({
-          size,
-          className:
-            "hover:dark:bg-neutral-800 hover:bg-slate-50 duration-75 transition-all hover:cursor-pointer ease-in-out",
-        })
-      )}
-    >
-      {children}
-    </div>
+    <>
+      <motion.div
+        initial={{
+          scale: 0.2,
+          y: 120,
+          opacity: 0,
+        }}
+        onClick={() => setOpen(true)}
+        className={cn(
+          variants({
+            size,
+            className:
+              "hover:dark:bg-neutral-800 hover:bg-slate-50 duration-75 transition-all hover:cursor-pointer ease-in-out",
+          })
+        )}
+      >
+        {children}
+      </motion.div>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            key="modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl shadow-xl dark:bg-neutral-900 dark:text-neutral-100 w-full sm:w-2/3 h-full sm:h-2/3 p-8 z-50"
+          >
+            {children}
+            <motion.button onClick={() => setOpen(false)}>
+              <Button size="icon" className="rounded-full" variant={"outline"}>
+                <X />
+              </Button>
+            </motion.button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
--- src/components/layout/grid-container.tsx
+++ src/components/layout/grid-container.tsx
@@ -4,10 +4,40 @@ import SocialItem from "../grid-items/social-item";
 import ExperienceItem from "../grid-items/experience-item";
 import ProjectItem from "../grid-items/project-item";
 import SkillItem from "../grid-items/skill-item";
+import { stagger, useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 const GridContainer = () => {
+  const [scope, animate] = useAnimate();
+  const staggerGridItems = stagger(0.02, {
+    startDelay: 0.5,
+  });
+
+  // ANIMATION
+  useEffect(() => {
+    if (scope.current) {
+      animate(
+        "div",
+        {
+          scale: 1,
+          y: 0,
+          opacity: 1,
+        },
+        {
+          type: "spring",
+          stiffness: 330,
+          damping: 35,
+          delay: staggerGridItems,
+        }
+      );
+    }
+  }, [scope]);
+
   return (
-    <div className="w-full h-full overflow-y-scroll scrollbar-none scrollbar-thumb-neutral-500 scrollbar-track-gray-100 p-4 sm:p-6 grid grid-cols-4 auto-rows-[76px] gap-3 sm:gap-6">
+    <div
+      ref={scope}
+      className="w-full h-full overflow-y-scroll scrollbar-none scrollbar-thumb-neutral-500 scrollbar-track-gray-100 p-4 sm:p-6 grid grid-cols-4 auto-rows-[76px] gap-3 sm:gap-6"
+    >
       {gridItems.map((item, index) => {
         return (
           <GridItem key={item.title + index} size={item.layout}>
